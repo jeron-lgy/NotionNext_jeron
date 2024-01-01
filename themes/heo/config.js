dcdfaa25b7f5986fd27da74cc0c9972b0ac5d02c
jeron-lgy <77041127+jeron-lgy@users.noreply.github.com>
2024-01-01 17:26:21 +0800

    config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,22 +5,22 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'jeron.life' },
+    { title: '访问文档中心获取更多帮助', url: 'https://www.behance.net/jeronli/appreciated' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '个人练习',
+  HEO_HERO_TITLE_2: '与项目案例',
+  HEO_HERO_TITLE_3: 'jeron.life',
+  HEO_HERO_TITLE_4: '欢迎欢迎',
+  HEO_HERO_TITLE_5: '不知道该改什么的占位符',
+  HEO_HERO_TITLE_LINK: 'https://www.behance.net/jeronli/appreciated',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/项目经历' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/个人练习' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -100,10 +100,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
+  HEO_SOCIAL_CARD_TITLE_1: '交流学习',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_3: '点击加我微信',
+  HEO_SOCIAL_CARD_URL: 'https://eturl.cn/UTiTvd',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
